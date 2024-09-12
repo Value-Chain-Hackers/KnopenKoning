a26cbdc6ed55f07937415c47aa1163a259b8e43f
root <root@controller.valuechainhackers.xyz>
2024-09-12 10:49:30 +0200

    added build_images and changed compose script

---
+++ build_images.sh
@@ -0,0 +1,3 @@
+docker buildx build -f Dockerfile.Frontend    -t vch_frontend:latest . 
+docker buildx build -f Dockerfile.Backend     -t vch_backend:latest . 
+docker buildx build -f Dockerfile.Scheduler   -t vch_scheduler:latest . 
